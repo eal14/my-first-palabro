37812940e8d5b39a771f1d56f98d2b36c61605ab
Eliseo <eliseo.alvarez.c@gmail.com>
2017-10-28 19:43:22 -0400

    Se agrega consulta de info a bd

--- palabro/views.py
+++ palabro/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Language
+
 # Create your views here.
 def palabro_list(request):
-    return render(request, 'palabro/palabro_list.html', {})
+    languages = Language.objects.all()
+    return render(request, 'palabro/language_list.html', {'languages': languages})
